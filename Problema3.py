774ebf6fad5350dfbd468c4f97d76622a3b2b421
NallelyPB <nallelyparedes6@gmail.com>
2024-09-22 05:18:46 +0000

    Subida del ejercicio 3

---
+++ Problema3.py
@@ -0,0 +1,12 @@
+'''Una juguetería tiene mucho éxito en dos de sus productos: payasos y muñecas. Suele hacer venta
+por correo y la empresa de logística les cobra por peso de cada paquete así que deben calcular el
+peso de los payasos y muñecas que saldrán en cada paquete a demanda. Cada payaso pesa 112 g y
+cada muñeca 75 g. Escribir un programa que lea el número de payasos y muñecas vendidos en el
+último pedido y calcule el peso total del paquete que será enviado.
+'''
+pesoM=75
+pesoP=112
+muñeca = float(input("Ingrese el numero de muñecas: "))
+payaso = float(input("Ingrese el numero de payasos: "))
+pesoTotal = (muñeca*pesoM)+(payaso*pesoP)
+print(f"El peso total en el último pedido es {pesoTotal}")
