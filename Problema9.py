c31db97be47302a5788f30410663b601550586e1
NallelyPB <nallelyparedes6@gmail.com>
2024-09-22 06:16:17 +0000

    Subida del problema 9

---
+++ Problema9.py
@@ -0,0 +1,10 @@
+'''Escriba un programa que, dada una lista, devuelve una nueva lista cuyo contenido sea igual a la
+original pero invertida. Así, dada la lista ['Di', 'buen', 'día', 'a', 'papa'], deberá devolver ['papa', 'a',
+'día', 'buen', 'Di'].'''
+
+
+lista_original = ['Di', 'buen', 'día', 'a', 'papa']
+
+lista_invertida = lista_original[::-1]
+
+print(lista_invertida)
